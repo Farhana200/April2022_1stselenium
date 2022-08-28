e9cb68d67d4795d720505bccedc06effca1bd9af
Farhana Rashid <farhanarashid31@gmail.com>
2022-08-28 01:48:30 -0500

    modified

--- src/main/java/farhana/GitTest4.java
+++ src/main/java/farhana/GitTest4.java
@@ -1,21 +1,24 @@
 package farhana;
 
-public class GitTest2 {
+public class GitTest4 {
 
+	
 	public static void main(String[] args) {
-		printme();
-		printme2();
+		printme6();
+		printme7();
 	}
- public static void printme() {
+ public static void printme6() {
 	 System.out.println("git test");
  }
  
- public static void printme2() {
+ public static void printme7() {
 	 System.out.println("git test1");
 	 System.out.println("git test2");
 	 System.out.println("git test3");
 	 System.out.println("git test4");
-	 System.out.println("git test9");
  }
  
+}
+
+
 }
